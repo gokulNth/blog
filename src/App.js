cfb6eea82ef51c1219f416e1e8062fb326dcfcc7
gokul-zt346 <gokulnth>
2021-01-08 21:57:32 +0530

    flex test mobile 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import Article from './Components/Article';
 
 function App() {
   return (
-    <div className='container'>
-      <div className='d-flex flex-lg-nowrap flex-sm-wrap flex-wrap'>
-        {[1, 2, 3, 4, 5].map((i) => (
-          <Article key={i} />
-        ))}
-      </div>
+    <div className='d-flex flex-lg-nowrap flex-sm-wrap flex-wrap'>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Article key={i} />
+      ))}
     </div>
   );
 }
