b91c2558ce1eedc53bb4691136038e2d1419b6b3
gokul-zt346 <gokulnth>
2021-01-08 19:01:40 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Article from './Components/Article';
+
 function App() {
   return (
-    <div>HI</div>
+    <div className='container'>
+      <div className='row row-cols-1 row-cols-md-2 g-4'>
+        {[1, 2, 3, 4, 5].map((i) => (
+          <Article key={i} />
+        ))}
+      </div>
+    </div>
   );
 }
 
